b4db51e72b5bd024d016a7a548026b2bfe48792d
ioiasff <arash.github@gmail.com>
2016-04-04 17:52:03 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -230,7 +230,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {152485254},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
